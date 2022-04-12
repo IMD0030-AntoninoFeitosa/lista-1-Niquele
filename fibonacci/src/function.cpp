bffd0cc42f1fc9dcbce5515a79158b3ca9f1537d
Niquele <niquelefiga12@gmail.com>
2022-04-11 21:55:23 -0300

    fibonacci e intervalo_5
    atualizacao

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,32 @@
 #include "function.h"
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
+std::vector<unsigned int> fib_below_n( unsigned int n ){
+std::vector <unsigned int> v;
+  
+bool condicao = true;
+int contador = 0;
+int posicao_n = 0;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+while(condicao){
+   if(contador == 0 || contador == 1){
+	  if (n>1){
+	    v.push_back(1);
+}
+	  else{
+	    return v;
+	}
+}
+  else{
+	  posicao_n = v[contador-1]+v[contador-2];
+	    if (posicao_n < n){	
+	      v.push_back(posicao_n);
+}
+	else{
+		condicao=false;	
+  }
+}
+  contador = contador +1;
+}
+  return v;
+  
 }
--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-using std::cin;
-using std::cout;
 
-const int SIZE = 5; // input size.
+int main() {
+  int n1;
+  int result = 0;
 
-int main(void)
-{
-    // TODO: Adicione aqui seu código.
-
-    return 0;
+  for (int i = 0; i < 5; i++){
+    std::cin >> n1;
+    if (n1 < 0){
+      result = result + 1;
+    }
+  }
+  std::cout << result; 
 }
