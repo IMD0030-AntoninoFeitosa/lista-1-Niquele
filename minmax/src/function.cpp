3062b5c1b7d44fe0f73af33f18a15ce30f5545e3
Niquele <niquelefiga12@gmail.com>
2022-04-13 21:16:19 -0300

    commit 5
    minmax

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -12,8 +12,28 @@
 
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
+  int j;
+  int i = 0;
+  for (i = 0; i < n; i++ ) {
+      std::cin>>V[i];
+   } 
 
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+  int menor_valor = V[0];
+  int maior_valor = V[0];
+  int menor_posicao = V[0];
+  int maior_posicao = V[0];
+  if (n==0){
+    return std::make_pair(-1,-1);
+  }
+  for(j = 0; j < n; j++) {
+        if(V[j] < menor_valor) {  
+            menor_valor = V[j];
+            menor_posicao = j;
+        }
+        if(V[j] >= maior_valor) { 
+            maior_valor = V[j];
+            maior_posicao = j;
+        } 
+    }
+   return std::make_pair(menor_posicao,maior_posicao); 
 }
