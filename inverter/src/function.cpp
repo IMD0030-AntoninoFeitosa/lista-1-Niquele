3af657d0240949f26fd96eacf6fdbe2dee391ad7
Niquele <niquelefiga12@gmail.com>
2022-04-13 21:41:02 -0300

    commit 7

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,31 +1,12 @@
-#include "function.h"
 #include <iostream>
 #include <array>
-#include <vector>
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
+/*! 
+ * Reverse de order of elements inside the array.
+ * @param arr Reference to the array with the values.
+ */
+template <std::size_t SIZE>
+void reverse( std::array< std::string, SIZE > & arr )
 {
-    int primeiro_termo = 0;
-    int segundo_termo = 1;
-    int n_termo = 0;
-  
-    std::vector<unsigned int> v;
-
-    for (int i = 1; i <= n; ++i) {
-        if(i == 1) {
-            v.push_back(primeiro_termo);
-            continue;
-        }
-        if(i == 2) {
-            v.push_back(segundo_termo);
-            continue;
-        }
-        n_termo = primeiro_termo + segundo_termo;
-        primeiro_termo = segundo_termo;
-        segundo_termo = n_termo;
-
-        v.push_back(n_termo);
-    }
-
-    return v;
+    /*codigo*/
 }
